ee94b41d8ddc491253484e2cd8dda25ba69ceda5
felix <langfe@cip.ifi.lmu.de>
2025-11-04 13:52:54 +0100

    nf core slurm

--- utils/mtx2h5.py
+++ utils/mtx2h5.py
@@ -23,6 +23,31 @@ def convert_mtx2h5(mtx_folder):
     test = sc.read_h5ad(os.path.join(mtx_folder, f"{prefix_mtx}merged.h5ad"))
     print(f"Test shape: {test.shape}")
 
+    
+def debug():
+    h5ad_file = "database/GSE216244_RAW/UPT/GSM6664614_UPT_merged.h5ad"
+
+    # Laden der AnnData-Datei
+    adata = sc.read_h5ad(h5ad_file)
+
+    # --- Überprüfung der Hauptstruktur ---
+    print("===================================")
+    print("AnnData Objekt Struktur:")
+    print(adata)
+
+    # --- Überprüfung der Zell-Metadaten (.obs) ---
+    print("\nZell-Metadaten (ersten 5 Zeilen):")
+    print(adata.obs.head())
+
+    # --- Überprüfung der Gen-Metadaten (.var) ---
+    print("\nGen-Metadaten (ersten 5 Zeilen):")
+    print(adata.var.head())
+
+    # --- Überprüfung der Zählmatrix-Dimensionen ---
+    print(f"\nDimensionen der Matrix: {adata.shape}")
+    print(f"Anzahl Zellen (obs): {adata.n_obs}")
+    print(f"Anzahl Gene (var): {adata.n_vars}")
+    print("===================================")
 
 def main():
     parser = argparse.ArgumentParser(description='Convert Mtx to h5')
@@ -37,3 +62,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    debug()
