b67fe9321ac0927564d14f33b3d0fa07c562cb7f
felix <langfe@cip.ifi.lmu.de>
2025-12-04 13:59:39 +0100

    plot umap 4 integration

--- tro_org/utils/plot_utils.py
+++ tro_org/utils/plot_utils.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import scanpy as sc
-import os
 
 def plot_umap_before_integration(adata, method, outdir, batch_key, label_key):
     save_dir = Path(f"{outdir}/{method}")
@@ -9,15 +8,14 @@ def plot_umap_before_integration(adata, method, outdir, batch_key, label_key):
     sc.tl.pca(adata)
     sc.pp.neighbors(adata)
     sc.tl.umap(adata)
-    sc.pl.umap(adata, color=label_key,save=f"umap_after_integration_{method}.png")
-    plt.savefig()
+    sc.pl.umap(adata, color=label_key, save=f"umap_after_integration_{method}.png")
     print("before")
     print(adata)
 
-def plot_umap_after_integration(adata, key, method, outdir, batch_key, label_key):
+def plot_umap_after_integration(adata, osbm_key, method, outdir, batch_key, label_key):
     save_dir = Path(f"{outdir}/{method}")
     sc.settings.figdir = save_dir
-    sc.pp.neighbors(adata, use_rep=key)
+    sc.pp.neighbors(adata, use_rep=osbm_key)
     sc.tl.umap(adata)
     sc.tl.leiden(adata, key_added=f"leiden_{method}", flavor="igraph", n_iterations=2)
     sc.pl.umap(adata, color=[label_key, batch_key, "leiden_scglue"], wspace=0.4, save=f"umap_after_integration_{method}.png")
