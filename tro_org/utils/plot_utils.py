932d0ae1267c053a380122b90555b5853d8d2295
felix <langfe@cip.ifi.lmu.de>
2025-12-04 14:30:05 +0100

    plot umap 4 integration

--- tro_org/utils/plot_utils.py
+++ tro_org/utils/plot_utils.py
@@ -7,13 +7,13 @@ def plot_umap_before_integration(adata, method, outdir, batch_key, label_key):
     sc.tl.pca(adata)
     sc.pp.neighbors(adata)
     sc.tl.umap(adata)
-    sc.pl.umap(adata, color=label_key, save=f"_after_integration_{method}.png")
+    sc.pl.umap(adata, color=label_key, save=f"_before_integration_{method}.png")
     print("before")
     print(adata)
 
 def plot_umap_after_integration(adata, osbm_key, method, outdir, batch_key, label_key):
-    #save_dir = Path(f"{outdir}/{method}")
-    #sc.settings.figdir = save_dir
+    save_dir = Path(f"{outdir}/{method}")
+    sc.settings.figdir = save_dir
     sc.pp.neighbors(adata, use_rep=osbm_key)
     sc.tl.umap(adata)
     sc.tl.leiden(adata, key_added=f"leiden_{method}", flavor="igraph", n_iterations=2)
