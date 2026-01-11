3ff1f8d7ba7bffc96893443b13ec164439f339d0
felix <langfe@cip.ifi.lmu.de>
2026-01-11 15:41:34 +0100

    benchmark dort plot color

--- tro_org/benchmark/plot_utils.py
+++ tro_org/benchmark/plot_utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 
 def plot_dotplot_benchmark(df, output_dir):
 
@@ -13,7 +14,7 @@ def plot_dotplot_benchmark(df, output_dir):
     bio_conservation = df.loc["Bio conservation", methods].astype(float)
 
     plt.figure(figsize=(7, 6))
-    plt.scatter(batch_correction, bio_conservation, c=["#383a6b"])
+    plt.scatter(batch_correction, bio_conservation, c=["#cb1f73"])
 
     for method in methods:
         plt.text(
@@ -25,15 +26,17 @@ def plot_dotplot_benchmark(df, output_dir):
             va="bottom"
         )
 
+    basename = os.path.basename(output_dir)
+
     plt.xlabel("Batch correction (aggregate score)")
     plt.ylabel("Biological conservation (aggregate score)")
-    plt.title("Integration methods: Batch correction vs Bio conservation")
+    plt.title(f"{basename} Benchmark: Batch correction vs Bio conservation")
 
     if "Unintegrated" in methods:
-        plt.axhline(bio_conservation["Unintegrated"], linestyle="--", linewidth=1)
-        plt.axvline(batch_correction["Unintegrated"], linestyle="--", linewidth=1)
+        plt.axhline(bio_conservation["Unintegrated"], linestyle="--", linewidth=1, color="#383a6b")
+        plt.axvline(batch_correction["Unintegrated"], linestyle="--", linewidth=1, color="#383a6b")
 
     plt.tight_layout()
-    plt.savefig(f"{output_dir}/dotplot_benchmark.png")
+    plt.savefig(f"{output_dir}/{basename}_dotplot_benchmark.png")
     plt.show()
 
