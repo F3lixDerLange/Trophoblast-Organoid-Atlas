60a6a67afa32db622b11d10e53934e4b052da5d4
felix <langfe@cip.ifi.lmu.de>
2026-02-04 17:32:58 +0100

    pyScenic CTX - AUCELL

--- tro_org/GRN/pyscenic.py
+++ tro_org/GRN/pyscenic.py
@@ -2,7 +2,7 @@
 import argparse
 import os
 from pathlib import Path
-
+import glob
 import numpy as np
 import pandas as pd
 import scanpy as sc
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 import subprocess
 
 # all TFs from https://resources.aertslab.org/cistarget/tf_lists/
+# all files downloaded from https://resources.aertslab.org/cistarget/
+# there is also an explanation which files to use
 
 def adata2loom(adata, loom_path):
     row_attrs = {
@@ -25,45 +27,108 @@ def adata2loom(adata, loom_path):
     lp.create(loom_path, adata.X.transpose(), row_attrs, col_attrs)
 
 
-def create_grn(adata, loom_path_scenic, data_dir, dataset, image=None):
-    adata2loom(adata, loom_path_scenic)
+def create_grn(adata, loom_path_scenic, data_dir, dataset, image=None, num_workers=20):
+
+    if not os.path.exists(os.path.join(data_dir, f"{dataset}_filtered_scenic.loom")):
+        adata2loom(adata, loom_path_scenic)
+    else:
+        print(f"Skip loom generation ---- {dataset}_filtered_scenic.loom already exists")
+
+    run_dir = Path(data_dir).resolve()
+    if image == "docker":
+        cmd = ["docker","run","--rm","--platform","linux/amd64","-v",f"{run_dir}:/data","aertslab/pyscenic:0.12.1"]
+
+    elif image == "singularity":
+        cmd = ["singularity", "run", "-B", f"{run_dir}:/data", f"{run_dir}/aertslab-pyscenic-0.12.1.sif"]
+
+    else:
+        raise SystemExit(f"Unknown image {image}")
+
 
     # STEP 1: Gene regulatory network inference, and generation of co-expression modules
     if not os.path.exists(os.path.join(data_dir, f"{dataset}_adj.tsv")):
-        run_dir = Path(data_dir).resolve()
         scenic_grn = [
             "pyscenic", "grn",
             f"/data/{dataset}_filtered_scenic.loom",
             "/data/allTFs_hg38.txt",
             "--method", "grnboost2",
-            "--num_workers", "30",
+            "--num_workers", f"{num_workers}",
             # "--sparse",
             "-o", f"/data/{dataset}_adj.tsv",
         ]
 
-        if image == "docker":
-            img = "aertslab/pyscenic:0.12.1"
-            cmd = [
-                             "docker", "run", "--rm",
-                             "--platform", "linux/amd64",
-                             "-v", f"{run_dir}:/data",
-                             img
-                         ] + scenic_grn
-
-        elif image == "singularity":
-            cmd = [   "singularity", "run",
-                                  "-B", f"{run_dir}:/data",
-                                  f"{run_dir}/aertslab-pyscenic-0.12.1.sif",
-                              ] + scenic_grn
-        else:
-            raise SystemExit(f"Unknown image {image}")
-
-        print("Running pyscenic command")
-        subprocess.run(cmd, check=True)
+        print("Running pyscenic GRN")
+        subprocess.run(cmd + scenic_grn, check=True)
         print("Done")
+    else:
+        print(f"Skip adj generation ---- {dataset}_adj.tsv already exists")
 
     adjacencies = pd.read_csv(f"{data_dir}/{dataset}_adj.tsv", index_col=False, sep='\t')
-    print(adjacencies.head())
+    print(adjacencies.tail())
+    print(f"Number of associations: {adjacencies.shape[0]}")
+
+    #plt.hist(np.log10(adjacencies["importance"]), bins=100)
+    #plt.xlim([-10, 10])
+    #plt.show()
+
+
+    # STEP 2-3: Regulon prediction aka cisTarget from CLI
+    f_db_glob = f"{data_dir}/*.feather"
+    f_db_names = ' '.join([os.path.join("/data", os.path.basename(i)) for i in glob.glob(f_db_glob)])
+    f_motif_path = "/data/motifs-v10nr_clust-nr.hgnc-m0.001-o0.0.tbl"
+
+    if not os.path.exists(os.path.join(data_dir, f"{dataset}_reg.csv")):
+        scenic_ctx = [
+            "pyscenic", "ctx",
+            f"/data/{dataset}_adj.tsv",
+            f"{f_db_names}",
+            f"--annotations_fname", f"{f_motif_path}",
+            "--expression_mtx_fname", f"/data/{dataset}_filtered_scenic.loom",
+            "--output", f"/data/{dataset}_reg.csv",
+            "--mask_dropouts",
+            "--num_workers", f"{num_workers}",
+        ]
+
+        print("Running pyscenic CTX")
+        subprocess.run(cmd + scenic_ctx, check=True)
+        print("Done")
+    else:
+        print(f"Skip ctx generation ---- {dataset}_reg.csv already exists")
+
+
+    #STEP 4: Cellular enrichment (aka AUCell) from CLI
+    n_genes_detected_per_cell = np.sum(adata.X > 0, axis=1)
+    percentiles = pd.Series(n_genes_detected_per_cell.flatten().A.flatten()).quantile(
+        [0.01, 0.05, 0.10, 0.50, 1]
+    )
+    print(percentiles)
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 5), dpi=150)
+    sns.histplot(n_genes_detected_per_cell, bins='fd', ax=ax, legend=False)
+    for i, x in enumerate(percentiles):
+        fig.gca().axvline(x=x, ymin=0, ymax=1, color='red')
+        ax.text(x=x, y=ax.get_ylim()[1], s=f'{int(x)} ({percentiles.index.values[i] * 100}%)', color='red', rotation=30,
+                size='x-small', rotation_mode='anchor')
+    ax.set_xlabel('# of genes')
+    ax.set_ylabel('# of cells')
+    fig.suptitle(f"Distribution of genes per cell in {dataset}", y=0.95)
+    fig.tight_layout()
+    plt.show()
+
+    if not os.path.exists(os.path.join(data_dir, f"{dataset}_pyscenic_output.loom")):
+        scenic_aucell = [
+            "pyscenic", "aucell",
+            f"/data/{dataset}_filtered_scenic.loom",
+            f"{dataset}_reg.csv",
+            "--output", f"{dataset}_pyscenic_output.loom",
+            "--num_workers", f"{num_workers}",
+        ]
+
+        print("Running pyscenic AUCELL")
+        subprocess.run(cmd + scenic_aucell, check=True)
+        print("Done")
+    else:
+        print(f"Skip ctx generation ---- {dataset}_pyscenic_output.loom already exists")
 
 
 def main():
