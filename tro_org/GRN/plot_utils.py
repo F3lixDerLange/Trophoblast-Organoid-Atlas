2e59c5fac47e1e63a0315e83d5e7a8cd7dcc7315
felix <langfe@cip.ifi.lmu.de>
2026-04-22 16:59:28 +0200

    small fixes

--- tro_org/GRN/plot_utils.py
+++ tro_org/GRN/plot_utils.py
@@ -131,9 +131,9 @@ def top_tf_per_celltype_scatter_allinone(adata_batch_top_tfs, dataset, outdir, l
     sns.scatterplot(data=plot_df, x="mean_expression", y="TF", hue=label_key, s=90, palette=COLORS)
     plt.xlabel("Mean TF expression")
     plt.title(f"TF expression across cell types \n{dataset}")
-    plt.legend(title="label", bbox_to_anchor=(1.02, 1), loc="upper left")
+    plt.legend(title="label", loc="upper left")
     plt.grid(True, axis="x", alpha=0.3)
-    plt.tight_layout()
+    #plt.tight_layout()
     plt.savefig(f"{outdir}/{dataset}/{dataset}_tf_mean_expression_celltype_allinone.png", dpi=150)
     plt.show()
 
